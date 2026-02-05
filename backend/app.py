4db82d1ad120055c08a838d627b644d5355fb3d0
nissam7 <150989399+nissam7@users.noreply.github.com>
2026-02-05 19:55:50 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
@@ -13,10 +15,15 @@ def get_db():
         connection_timeout=5
     )
 
+# GET → show HTML
 @app.route("/", methods=["GET"])
 def index():
-    return send_from_directory("frontend", "1.html")
+    return send_from_directory(
+        os.path.join(BASE_DIR, "frontend"),
+        "1.html"
+    )
 
+# POST → save data
 @app.route("/", methods=["POST"])
 def submit():
     db = get_db()
